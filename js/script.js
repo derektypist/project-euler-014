5abc87bb9d874264b060731bbbdb75b6192a2782
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-04-28 10:23:55 +0000

    Added longestCollatzSequence, colLen and clearInfo functions at script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,45 @@ function getNumberInfo() {
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
 }
+
+/*
+    Function to return the number under the limit that generates the
+    longest sequence using the formula:
+
+        n -> n/2 (n is even)
+        n -> 3n+1 (n is odd)
+    
+        longestCollatzSequence(14) returns 9
+        longestCollatzSequence(1000000) returns 837799
+*/
+function longestCollatzSequence(limit) {
+    let longest = 1;
+    let maxLength = 1;
+    for (let i=Math.floor(limit/2);i<limit;i++) {
+        let len = colLen(i);
+        if (len>maxLength) {
+            longest = i;
+            maxLength = len;
+        }
+    }
+    return longest;
+}
+
+const knownSequence = {'1':1};
+
+// Function to Return the Length of the Collatz Sequence
+function colLen(n) {
+    if (knownSequence[n]) {
+        return knownSequence[n];
+    } else {
+        const len = n%2 === 0 ? colLen(n/2) + 1 : colLen((3*n+1)/2) + 2;
+        knownSequence[n] = len;
+        return len;
+    }
+}
+
+// Function to Clear Information
+function clearInfo() {
+    let txt="";
+    document.getElementById("numinfo").innerHTML = txt;
+}
