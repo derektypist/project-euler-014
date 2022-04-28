f281fe06404e8654cdb5ea0bb4098cae55d43012
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-04-28 10:38:38 +0000

    Updated getNumberInfo function - number range at script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function getNumberInfo() {
     // Get the Value of the Input Field
     let num = document.getElementById("mynumber").value;
     // Check if the input is valid
-    if (isNaN(num) || num.toString().length == 0 || num<1 || num>2000000 || !Number.isInteger(Number(num))) {
-        txt += `Invalid Input.  Please enter a whole number between 1 and 2000000.`;
+    if (isNaN(num) || num.toString().length == 0 || num<2 || num>2000000 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter a whole number between 2 and 2000000.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;
         txt += `The starting number, under ${num}, produces the longest chain is ${longestCollatzSequence(num)}.`;
